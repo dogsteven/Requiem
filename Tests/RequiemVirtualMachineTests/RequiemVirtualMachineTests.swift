e0c33eed8bd56be6301657a6b82dbc236b1bf1d4
dogsteven <caigicungduoc23@gmail.com>
2023-12-19 20:57:05 +0700

    Initial Commit

---
+++ Tests/RequiemVirtualMachineTests/RequiemVirtualMachineTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import RequiemVirtualMachine
+
+final class RequiemVirtualMachineTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
